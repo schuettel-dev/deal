7f73098ba0d24bfc8e2fef1f98e9208e1d5c57c4
mario <github@lxxxvi.ch>
2021-09-08 13:04:25 +0200

    wip, added/configured tailwind manually

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    "./app/**/*.html.haml",
+    "./app/components/*.html.haml",
+    "./app/components/*.rb",
+    "./app/javascript/styles/**/*.css",
+    "./app/javascript/controllers/**/*.js"
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
